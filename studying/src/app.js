4d68c3ee32868f34ca22473bfbe6bf093ac28063
WinterKwon <winterkwon7017@gmail.com>
2022-05-12 01:23:02 +0900

    src폴더로 소스코드 분류

---
+++ studying/src/app.js
@@ -0,0 +1,27 @@
+"use strict"
+
+//login 화면만들기
+//express 서버 만든 것 가져오기
+
+
+
+const express = require("express");
+const app = express();
+const PORT = 3003;  //직접 포트번호 하드코딩하지 않기!
+//앱 세팅
+// views/home 폴더 만들고 그 안에 index.ejs, login.ejs 파일만들기
+//npm install ejs -s 
+
+app.set("views", "./views"); //views는 studying/views를 기본 경로로 하겠다
+app.set("view engine", "ejs");  //뷰 엔진은 ejs로 하겠다 -> npm install  필요
+
+const home = require("./routes/home");  // 루트 경로로 오면 home으로 이동하도록 경로 설정
+app.use("/", home);  //.use()는 middleware등록 매서드
+
+
+//bin/www/js로 분리
+// app.listen(PORT, ()=> {
+//     console.log("서버 가동");
+// });
+
+module.exports = app;
