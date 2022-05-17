5c334bdfec7f260956b5593514d3fbddeab8242d
WinterKwon <winterkwon7017@gmail.com>
2022-05-17 21:50:26 +0900

    DOM으로 HTML 객체 제어하기 -login

--- studying/src/public/javascript/home/login.js
+++ studying/src/public/javascript/home/login.js
@@ -1,3 +1,21 @@
 "use strict"
 
-console.log("hello");
+//front처리
+// 로그인 버튼 클릭시 서버로 데이터 보내기
+
+const id = document.querySelector('#id'),
+ pw = document.querySelector('#password'),
+ loginBtn = document.querySelector('button');
+
+// console.log(id); //defer 기능 확인하기
+loginBtn.addEventListener('click', login);
+
+async function login(){
+        // console.log("bye");
+    const req = await {
+        id : id.value,
+        password : pw.value
+    }
+
+    console.log(req);
+}
