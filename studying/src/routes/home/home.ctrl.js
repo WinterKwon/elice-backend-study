4610abdb5f7d320605f97a0148ab7eaffd820d96
WinterKwon <winterkwon7017@gmail.com>
2022-05-18 00:33:14 +0900

    UserStorage 만들기

--- studying/src/routes/home/home.ctrl.js
+++ studying/src/routes/home/home.ctrl.js
@@ -18,6 +18,7 @@
 // // {hello : hello, login : login}과 같다
 
 // get일때와 post일때의 라우터가 잘 구분되지 않아 가독성 있게 변경
+const UserStorage = require('../../models/UserStorage');
 
 const output = {
     hello : (req,res) =>{
@@ -38,30 +39,33 @@ const process = {
         //id,password 검증
         const id = req.body.id,
             password = req.body.password;
-        console.log(id, password);
-
+        
+        // const UserStorage = new UserStorage();  //models의 클래스에서 변수에 static 선언하면 이 과정은 필요 없다. 
+        console.log(UserStorage.getUsers("id", "password"))  // 이렇게 외부에서 불러오는 걸 막으려면 은닉화 -> private으로 선언해줘야함
+        const users = UserStorage.getUsers("id", "password");
+     
+        const response = {}
         if ( users.id.includes(id)){
             const idx = users.id.indexOf(id);
             if(users.password[idx] == password){
-                return res.json({
-                    success : true
-                })
+                response.success = true;
+                return res.json(response)
 
             }
 
         }
-
-        return res.json({
-                success  :false,
-                msg : "로그인에 실패하셨습니다."
-        });
+        response.success = false;
+        response.msg = '로그인에 실패했습니다';
+        return res.json(response);
     }
 }
 
 //login인증기능위해선 서버가 미리 사용자의 정보 가지고 있어야 함
-const users = {
-    id : ["wayo7813", "pjkwon","doctor","ceoKwon","dragonKwon"],
-    password : ["1234","1111","2345","3456","4567"]
-}
+// const users = {
+//     id : ["wayo7813", "pjkwon","doctor","ceoKwon","dragonKwon"],
+//     password : ["1234","1111","2345","3456","4567"]
+// }
+//models로 이관
+
 
 module.exports = {output, process};
